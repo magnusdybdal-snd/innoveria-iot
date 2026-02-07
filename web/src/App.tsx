33706a7ce233e8b1fc889a9a13400fa95fea4270
Jonas <jonnyga@ntnu.no>
2026-02-07 18:35:36 +0100

    style: color scheme
    Brukte fargene fra figma-figurene

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -7,28 +7,37 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <>
+          <div id="top">
+              <h3>Header logo</h3>
+              <br/>
+              <ul id="navigation">
+                  <li><a href="front.html">Header link one</a></li>
+                  <li><a href="#">Header link two</a></li>
+                  <li><a href="page3.html">Header link three</a></li>
+              </ul>
+          </div>
+          <div>
+          <a href="https://vite.dev" target="_blank">
+                  <img src={viteLogo} className="logo" alt="Vite logo"/>
+              </a>
+              <a href="https://react.dev" target="_blank">
+                  <img src={reactLogo} className="logo react" alt="React logo"/>
+              </a>
+          </div>
+          <h1>Vite + React</h1>
+          <div className="card">
+              <button onClick={() => setCount((count) => count + 1)}>
+                  count is {count}
+              </button>
+              <p>
+                  Edit <code>src/App.tsx</code> and save to test HMR
+              </p>
+          </div>
+          <p className="read-the-docs">
+              Click on the Vite and React logos to learn more
+          </p>
+      </>
   )
 }
 
