2a1b6c8ab872e7ed958abf94d95754b7d983e8ae
Jonas <jonnyga@ntnu.no>
2026-02-08 18:19:50 +0100

    feat: navigasjons-bar struktur
    Strukturen til navigasjonsbaren inkludert logo, teks og log ut knapp

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
+import innLogo from './assets/innoveria.png'
 import viteLogo from '/vite.svg'
 import './App.css'
 
@@ -9,34 +10,43 @@ function App() {
   return (
       <>
           <div id="top">
-              <h3>Header logo</h3>
-              <br/>
               <ul id="navigation">
-                  <li><a href="front.html">Header link one</a></li>
-                  <li><a href="#">Header link two</a></li>
-                  <li><a href="page3.html">Header link three</a></li>
+                  <li><a href="#" target="_blank">
+                      <img src={innLogo} alt="Innoveria logo"/>
+                  </a></li>
+                  <li>Home</li>
+                  <li>Dashboard</li>
+                  <li>Sensors</li>
+                  <li>Settings</li>
+                  <li>
+                      <button>
+                          Log out
+                      </button>
+                  </li>
               </ul>
           </div>
-          <div>
-          <a href="https://vite.dev" target="_blank">
-                  <img src={viteLogo} className="logo" alt="Vite logo"/>
-              </a>
-              <a href="https://react.dev" target="_blank">
-                  <img src={reactLogo} className="logo react" alt="React logo"/>
-              </a>
-          </div>
-          <h1>Vite + React</h1>
-          <div className="card">
-              <button onClick={() => setCount((count) => count + 1)}>
-                  count is {count}
-              </button>
-              <p>
-                  Edit <code>src/App.tsx</code> and save to test HMR
+          <a padding="2em">
+              <div>
+                  <a href="https://vite.dev" target="_blank">
+                      <img src={viteLogo} className="logo" alt="Vite logo"/>
+                  </a>
+                  <a href="https://react.dev" target="_blank">
+                      <img src={reactLogo} className="logo react" alt="React logo"/>
+                  </a>
+              </div>
+              <h1>Vite + React</h1>
+              <div className="card">
+                  <button onClick={() => setCount((count) => count + 1)}>
+                      count is {count}
+                  </button>
+                  <p>
+                      Edit <code>src/App.tsx</code> and save to test HMR
+                  </p>
+              </div>
+              <p className="read-the-docs">
+                  Click on the Vite and React logos to learn more
               </p>
-          </div>
-          <p className="read-the-docs">
-              Click on the Vite and React logos to learn more
-          </p>
+          </a>
       </>
   )
 }
