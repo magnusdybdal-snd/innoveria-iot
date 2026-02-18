0e4b9ba6f958ffcbc075b143dc0d836c6f893a70
maeilert <maeilert@stud.ntnu.no>
2026-02-18 11:11:35 +0100

    feat: category header for gateways

---
+++ web/src/components/CategoryHeader/categoryHeader.tsx
@@ -0,0 +1,45 @@
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import type { ReactNode } from "react";
+
+type CategoryHeaderProps = {
+  categories: string[];
+  columns?: number;
+  children?: ReactNode;
+};
+
+export function CategoryHeader({
+  categories,
+  columns,
+  children,
+}: CategoryHeaderProps) {
+  // Creates number of columns based on string[] passed as parameter
+  // First category is made to fit the object through "auto"
+  const templateColumns = columns
+    ? `auto ${Array(columns - 1)
+        .fill("1fr")
+        .join(" ")}`
+    : `repeat(${categories.length}, 1fr)`;
+
+  return (
+    <Box
+      sx={{
+        display: "grid",
+        gridTemplateColumns: templateColumns,
+        columnGap: 4,
+        backgroundColor: "primary.light",
+        color: "primary.main",
+        padding: 2,
+      }}
+      className="w-full"
+    >
+      {/* Map every category to display as text */}
+      {categories.map((category) => (
+        <Typography key={category} className="px-4 py-2">
+          {category}
+        </Typography>
+      ))}
+      {children}
+    </Box>
+  );
+}
--- web/src/pages/Gateways.tsx
+++ web/src/pages/Gateways.tsx
@@ -2,6 +2,12 @@ import { SubPageHeader } from "@/components/subPageHeader";
 import Menu from "@/Menu";
 import { PageDivider } from "@/components/pageDivider";
 import { PageContent } from "@/components/pageContent";
+import { CategoryHeader } from "@/components/CategoryHeader";
+
+{
+  /* Categoriesdisplayed in header of gateways */
+}
+const gatewayDetails: string[] = ["Status", "Name", "EUI", "Last seen"];
 
 export default function Gateways() {
   return (
@@ -10,6 +16,10 @@ export default function Gateways() {
       <PageContent>
         <SubPageHeader title="Gateways" />
         <PageDivider />
+        <CategoryHeader
+          categories={gatewayDetails}
+          columns={gatewayDetails.length}
+        />
       </PageContent>
     </div>
   );
