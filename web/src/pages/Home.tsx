fe75ad620ffa7e7d05ffcc67e9a25f6131a2b532
maeilert <maeilert@stud.ntnu.no>
2026-02-18 15:55:04 +0100

    fix: remove redundant code from merge request with dev

--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -1,8 +1,5 @@
-import { AddBox } from "@/components/addCompBox";
 import { PageContent } from "@/components/pageContent";
 import { SubPageHeader } from "@/components/subPageHeader";
-import Box from "@mui/material/Box";
-import { Path } from "@/components/path";
 import Menu from "@/Menu.tsx";
 import { JokeViewer } from "@/API/getJoke";
 import { ApiViewer } from "@/API/getData/";
@@ -13,28 +10,9 @@ export default function Home() {
       <Menu />
       <PageContent>
         <SubPageHeader />
-        <AddBox />
+        <JokeViewer />
+        <ApiViewer />
       </PageContent>
-      <Box
-        sx={{
-          backgroundColor: "primary.dark",
-          color: "primary.main",
-        }}
-        className="flex-1 overflow-auto"
-      >
-        <Box
-          sx={{
-            paddingLeft: 2,
-            paddingRight: 2,
-            paddingTop: 2,
-          }}
-          className="flex-1 overflow-auto"
-        >
-          <Path />
-          <JokeViewer />
-          <ApiViewer />
-        </Box>
-      </Box>
     </div>
   );
 }
