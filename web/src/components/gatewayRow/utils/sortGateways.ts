2bdf8f60e17d48ab3768443ddf0361240142d991
maeilert <maeilert@stud.ntnu.no>
2026-02-19 13:11:02 +0100

    feat: sort devices by name, lastseen & status

--- web/src/components/gatewayRow/utils/sortGateways.ts
+++ web/src/components/gatewayRow/utils/sortGateways.ts
@@ -3,6 +3,8 @@ import type { Gateway } from "@/mocks/gateways";
 export type SortDirection = "asc" | "desc";
 export type GatewaySortKey = "Name" | "Status" | "Last seen";
 
+// Tuple with units to seconds.
+// Used to unify unit that is being used to sort in @sortGateway.
 const unitToSeconds: Record<string, number> = {
   sec: 1,
   min: 60,
@@ -12,11 +14,13 @@ const unitToSeconds: Record<string, number> = {
   days: 86400,
 };
 
+//
 function parseLastSeenToSeconds(lastSeen: string): number {
   const [amount, unit] = lastSeen.split(" ");
   return (parseInt(amount, 10) || 0) * (unitToSeconds[unit] ?? 0);
 }
 
+// Sorting gateways based on Key (GatewaySortkeys)
 export function sortGateways(
   gateways: Gateway[],
   key: GatewaySortKey | null,
