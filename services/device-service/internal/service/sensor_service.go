6b7a4875b928eaa427e2371bc41c5a90f838e8cb
vinjar <vinjar.dev@gmail.com>
2026-02-21 16:42:56 +0100

    Feat: Setting up sensor service in device service

--- services/device-service/internal/service/sensor_service.go
+++ services/device-service/internal/service/sensor_service.go
@@ -1,7 +1,20 @@
 package service
 
-import "innoveria-iot/device-service/internal/chirpstackrest"
+import (
+	"innoveria-iot/device-service/internal/chirpstackrest"
+	"innoveria-iot/device-service/internal/domain"
+)
 
 type SensorServiceImpl struct {
 	cc *chirpstackrest.Client
+}
+
+func NewSensorService(cc *chirpstackrest.Client) *SensorServiceImpl {
+	return &SensorServiceImpl{
+		cc: cc,
+	}
+}
+
+func (s *SensorServiceImpl) GetAll() ([]domain.Sensor, error) {
+	return nil, nil
 }
