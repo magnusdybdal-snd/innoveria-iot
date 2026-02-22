d7903dbfe50870407243711b13f0e97a4126fc75
vinjar <vinjar.dev@gmail.com>
2026-02-22 22:27:51 +0100

    Fix: Small typo in sensor impl - sensors -> sensor

--- services/device-service/internal/service/sensor_service.go
+++ services/device-service/internal/service/sensor_service.go
@@ -27,8 +27,8 @@ func (s *SensorServiceImpl) GetAll(ctx context.Context) ([]domain.Sensor, error)
 	}
 	var result []domain.Sensor
 
-	for _, sensors := range resp.Result {
-		result = append(result, mapSensor(sensors))
+	for _, sensor := range resp.Result {
+		result = append(result, mapSensor(sensor))
 	}
 	// 3. merge to sensor domain
 
