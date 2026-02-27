2303a1ae65144009466f295d29181369e8b7c971
Magnus Dybdal <magndy@stud.ntnu.no>
2026-02-27 21:51:58 +0100

    feat: add description fields and chirpstack application id to schema

---
+++ services/device-service/internal/db/migrations/20260227204616_add_description_and_application_id.sql
@@ -0,0 +1,34 @@
+-- +goose Up
+ALTER TYPE "device"."device_status"
+    RENAME TO "device_state";
+
+ALTER TABLE "device"."company_config"
+    ADD COLUMN "chirpstack_application_id" varchar UNIQUE NOT NULL;
+
+ALTER TABLE "device"."gateway"
+    ADD COLUMN "description" varchar;
+ALTER TABLE "device"."gateway"
+    RENAME COLUMN "status" TO "state";
+
+ALTER TABLE "device"."sensor"
+    ADD COLUMN "description" varchar;
+ALTER TABLE "device"."sensor"
+    RENAME COLUMN "status" TO "state";
+
+
+-- +goose Down
+ALTER TABLE "device"."sensor"
+    RENAME COLUMN "state" TO "status";
+ALTER TABLE "device"."sensor"
+    DROP COLUMN "description";
+
+ALTER TABLE "device"."gateway"
+    RENAME COLUMN "state" TO "status";
+ALTER TABLE "device"."gateway"
+    DROP COLUMN "description";
+
+ALTER TABLE "device"."company_config"
+    DROP COLUMN "chirpstack_application_id";
+
+ALTER TYPE "device"."device_state"
+    RENAME TO "device_status";
