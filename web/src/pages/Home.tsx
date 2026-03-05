28c6dda8aaf93784f39ceea094243a47e85ede5c
Jonas <jonnyga@ntnu.no>
2026-03-05 09:25:09 +0100

    feat: add new menu to all pages

--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -1,17 +1,17 @@
 import { JokeViewer } from "@/API/getJoke";
 import { PageContent } from "@/components/pageContent";
 import { SubPageHeader } from "@/components/subPageHeader";
-import Menu2 from "@/Menu2.tsx";
+import Menu from "@/Menu.tsx";
 
 export default function Home() {
   return (
-    <Menu2>
+    <Menu>
       <div className="flex h-screen">
         <PageContent>
           <SubPageHeader />
           <JokeViewer />
         </PageContent>
       </div>
-    </Menu2>
+    </Menu>
   );
 }
