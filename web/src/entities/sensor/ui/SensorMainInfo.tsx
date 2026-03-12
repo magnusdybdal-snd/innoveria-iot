bea8a5e6574890843d164d7379c886a9779b53f4
Jonas <jonnyga@ntnu.no>
2026-03-12 18:26:30 +0100

    feat: use in sensor

--- web/src/entities/sensor/ui/SensorMainInfo.tsx
+++ web/src/entities/sensor/ui/SensorMainInfo.tsx
@@ -2,15 +2,11 @@ import { useState } from "react";
 
 import CircleIcon from "@mui/icons-material/Circle";
 import Button from "@mui/material/Button";
-import Dialog from "@mui/material/Dialog";
-import DialogActions from "@mui/material/DialogActions";
-import DialogContent from "@mui/material/DialogContent";
-import DialogTitle from "@mui/material/DialogTitle";
 import { useTheme } from "@mui/material/styles";
-import TextField from "@mui/material/TextField";
 import Typography from "@mui/material/Typography";
 import { ActionMenu } from "@shared/ui/actionMenu";
 import { DeleteConfirmation } from "@shared/ui/DeleteConfirmation";
+import { RenameDialog } from "@shared/ui/RenameDialog";
 
 type InfoMainProps = {
   name: string;
@@ -106,25 +102,15 @@ export function SensorMainInfo({
         Extra sensor info
       </Button>
       <ActionMenu items={menuItems} />
-      <Dialog open={editOpen} onClose={() => setEditOpen(false)}>
-        <DialogTitle>Rename sensor</DialogTitle>
-        <DialogContent>
-          <TextField
-            autoFocus
-            value={editValue}
-            onChange={(e) => setEditValue(e.target.value)}
-            label="Sensor name"
-            fullWidth
-            sx={{ mt: 1 }}
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={() => setEditOpen(false)}>Cancel</Button>
-          <Button variant="contained" onClick={handleEditSave}>
-            Save
-          </Button>
-        </DialogActions>
-      </Dialog>
+      <RenameDialog
+        open={editOpen}
+        value={editValue}
+        onChange={setEditValue}
+        onClose={() => setEditOpen(false)}
+        onSave={handleEditSave}
+        label="Gateway name"
+        title="Rename gateway"
+      />
       <DeleteConfirmation
         open={deleteOpen}
         onClose={() => setDeleteOpen(false)}
