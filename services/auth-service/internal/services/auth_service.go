79dce82dae0e812ad1c271db270920be43baa0c6
vinjar <vinjar.dev@gmail.com>
2026-03-17 22:49:23 +0100

    feat: setting up auth with correct dependecies

--- services/auth-service/internal/services/auth_service.go
+++ services/auth-service/internal/services/auth_service.go
@@ -1,14 +1,34 @@
 // Package services contains auth-service business logic.
 package services
 
-import "context"
+import (
+	"context"
+	"time"
+
+	"innoveria-iot/auth-service/internal/domain"
+)
 
 // AuthServiceImpl implements authentication use cases for the auth service.
-type AuthServiceImpl struct{}
+type AuthServiceImpl struct {
+	userRepo  domain.UserRepo
+	jwtSecret []byte // converted to byte in initializer
+	jwtIssuer string
+	accessTTL time.Duration
+}
 
 // NewAuthServiceImpl creates a new AuthServiceImpl instance.
-func NewAuthServiceImpl() *AuthServiceImpl {
-	return &AuthServiceImpl{}
+func NewAuthServiceImpl(
+	userRepo domain.UserRepo,
+	jwtSecret string,
+	jwtIssuer string,
+	accessTTL time.Duration,
+) *AuthServiceImpl {
+	return &AuthServiceImpl{
+		userRepo:  userRepo,
+		jwtSecret: []byte(jwtSecret),
+		jwtIssuer: jwtIssuer,
+		accessTTL: accessTTL,
+	}
 }
 
 // Login authenticates a user.
