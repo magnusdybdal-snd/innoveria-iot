f0104d2ad80e53755b789a4b3c48e79ec03c02fa
vinjar <vinjar.dev@gmail.com>
2026-03-19 21:51:15 +0100

    fix: removed debugging message

--- services/api-gateway/internal/server/middleware.go
+++ services/api-gateway/internal/server/middleware.go
@@ -22,6 +22,7 @@ func corsMiddleware(next http.Handler) http.Handler {
 		}
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
 
 		if r.Method == http.MethodOptions {
 			w.WriteHeader(http.StatusNoContent)
