473cb82f8676d591f402ee061e0a6bca14ba4a14
Jonas <jonnyga@ntnu.no>
2026-03-20 12:16:29 +0100

    refactor: remove unused import

--- web/src/pages/Login.tsx
+++ web/src/pages/Login.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import { useContext, useState } from "react";
 
 import innLogoDark from "@assets/innoveriaDark.png";
 import innLogoLight from "@assets/innoveriaLight.png";
