bb78f5311f7e17a174a9b4cff34c1d1f9e4f77ec
vinjar <vinjar.dev@gmail.com>
2026-03-26 14:10:43 +0100

    feat: added monitor erp enviroment variables

--- services/erp-agent-service/internal/config/config.go
+++ services/erp-agent-service/internal/config/config.go
@@ -8,6 +8,14 @@ import (
 // Config holds erp-agent-service runtime configuration.
 type Config struct {
 	Addr string
+
+	MonitorERPHost          string // Monitor ERP host address
+	MonitorERPCompanyNumber string // Monitor ERP company number
+
+	MonitorERPUsername string // Monitor ERP username
+	MonitorERPPassword string // Monitor ERP password
+
+	MonitorERPForceRelogin bool // Should be false; true logs out all active Monitor ERP sessions
 	// EnableSwagger bool
 }
 
@@ -15,5 +23,11 @@ type Config struct {
 func Load() *Config {
 	return &Config{
 		Addr: ":" + env.Get("PORT", "8080"),
+
+		MonitorERPHost:          env.Get("MONITOR_ERP_HOST", ""),
+		MonitorERPCompanyNumber: env.Get("MONITOR_ERP_COMPANY_NUMBER", ""),
+		MonitorERPUsername:      env.Get("MONITOR_ERP_USERNAME", ""),
+		MonitorERPPassword:      env.Get("MONITOR_ERP_PASSWORD", ""),
+		MonitorERPForceRelogin:  env.GetBool("MONITOR_ERP_FORCE_RELOGIN", false),
 	}
 }
