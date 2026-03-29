f767fb18efa8f9303e9944570ffc2f94cbce0556
vinjar <vinjar.dev@gmail.com>
2026-03-29 15:20:05 +0200

    feat: setting up basic structure for http client

--- services/erp-agent-service/internal/monitor/client.go
+++ services/erp-agent-service/internal/monitor/client.go
@@ -1,6 +1,9 @@
 package monitor
 
-import "innoveria-iot/pkg/httpclient"
+import (
+	"fmt"
+	"innoveria-iot/pkg/httpclient"
+)
 
 type Client struct {
 	host, lang, company string
@@ -9,4 +12,22 @@ type Client struct {
 	sessionID           string
 }
 
-func (c *Client) ensureSession() error
+// base returns the base url for accessing monitor erp
+func (c *Client) base() string {
+	return fmt.Sprintf("https://%s:8001/%s/%s", c.host, c.lang, c.company)
+}
+
+// loginUrl returns the url for authenticating with monitor erp
+// it will return a session id which is used throughout the api calls
+func (c *Client) loginUrl() string {
+	return c.base() + "/login"
+}
+
+// apiUrl returns the endpoint for
+func (c *Client) apiUrl(path string) string {
+	return c.base() + "/api/v1/" + path
+}
+
+func (c *Client) ensureSession() error {
+	return nil
+}
