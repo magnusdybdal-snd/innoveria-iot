718a40348f35864213a1b9b5e6fc19b758ca09ec
Magnus Dybdal <magndy@stud.ntnu.no>
2026-03-30 14:21:26 +0200

    fix: linting error

--- services/collection-service/internal/server/router.go
+++ services/collection-service/internal/server/router.go
@@ -24,7 +24,7 @@ func NewRouter(svc domain.MeasurementService, tenantMappingSvc domain.TenantMapp
 
 	// Payload tags
 	mux.HandleFunc("GET "+PAYLOAD_TAGS, handlers.HandlePayloadTags(svc))
-	
+
 	// Company config routes:
 	mux.HandleFunc("POST "+COMPANY_CONFIG_ROUTE, handlers.PostTenantMapping(tenantMappingSvc))
 
