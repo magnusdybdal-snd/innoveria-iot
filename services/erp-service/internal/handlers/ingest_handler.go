7745ad6d22f438d757242280e0ef609f8c360d36
vinjar <vinjar.dev@gmail.com>
2026-04-10 16:59:13 +0200

    feat: setting up db and handler layer

---
+++ services/erp-service/internal/handlers/ingest_handler.go
@@ -0,0 +1,12 @@
+package handlers
+
+import (
+	"context"
+	"net/http"
+)
+
+func PostMonitorIngest(ctx context.Context) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+	}
+}
