5358d9bd97b1f287e119be90e3aeba165715503e
vinjar <vinjar.dev@gmail.com>
2026-04-14 22:14:41 +0200

    feat: adding db deps, and scaffold repository

--- services/erp-service/internal/repository/ingest_repo.go
+++ services/erp-service/internal/repository/ingest_repo.go
@@ -1,1 +1,36 @@
 package repository
+
+import (
+	"context"
+
+	"innoveria-iot/pkg/dbutil"
+)
+
+// IngestRepoImpl is the implementaion of ingest persistant storage write
+// Which is responsible for writing erp data from a erp agent service
+type IngestRepoImpl struct {
+	db *dbutil.DB
+}
+
+// NewIngestRepo initializes a new Ingest repository.
+func NewIngestRepo(db *dbutil.DB) *IngestRepoImpl {
+	return &IngestRepoImpl{
+		db: db,
+	}
+}
+
+func (r *IngestRepoImpl) CreateOrder(ctx context.Context) error {
+	return nil
+}
+
+func (r *IngestRepoImpl) CreateOrderOperation(ctx context.Context) error {
+	return nil
+}
+
+func (r *IngestRepoImpl) CreateOrderReport(ctx context.Context) error {
+	return nil
+}
+
+func (r *IngestRepoImpl) CreateProductionResource(ctx context.Context) error {
+	return nil
+}
