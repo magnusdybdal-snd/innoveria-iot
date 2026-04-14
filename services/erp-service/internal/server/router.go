3f2d0ecdee27f733a839dd53192afc04edba32fe
vinjar <vinjar.dev@gmail.com>
2026-04-14 19:19:08 +0200

    fix: router endpoints being swapped

--- services/erp-service/internal/server/router.go
+++ services/erp-service/internal/server/router.go
@@ -14,10 +14,10 @@ func NewRouter(ingestSvc domain.Ingest) *http.ServeMux {
 	mux.HandleFunc("GET /", handlers.Root)
 
 	// ERP AGENT SERVICE Endpoints
-	mux.HandleFunc("POST "+ORDER_ROUTE, handlers.PostIngestWorkCenters(ingestSvc))
-	mux.HandleFunc("POST "+ORDER_OPERATIONS_ROUTE, handlers.PostIngestOrder(ingestSvc))
-	mux.HandleFunc("POST "+ORDER_REPORTINGS_ROUTE, handlers.PostIngestOrderOperations(ingestSvc))
-	mux.HandleFunc("POST "+WORKCENTERS_ROUTE, handlers.PostIngestOrderReports(ingestSvc))
+	mux.HandleFunc("POST "+ORDER_ROUTE, handlers.PostIngestOrder(ingestSvc))
+	mux.HandleFunc("POST "+ORDER_OPERATIONS_ROUTE, handlers.PostIngestOrderOperations(ingestSvc))
+	mux.HandleFunc("POST "+ORDER_REPORTINGS_ROUTE, handlers.PostIngestOrderReports(ingestSvc))
+	mux.HandleFunc("POST "+WORKCENTERS_ROUTE, handlers.PostIngestWorkCenters(ingestSvc))
 
 	// Swagger docs
 	// mux.HandleFunc("GET /swagger/", httpSwagger.WrapHandler)
