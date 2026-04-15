5a546f72913335674e927e434183c441b1800913
maeilert <maeilert@stud.ntnu.no>
2026-04-15 07:53:26 +0200

    feat: InfoWidget to show values/data as text

--- web/src/pages/OrderContext.tsx
+++ web/src/pages/OrderContext.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 
+import { InfoWidget } from "@/widgets/infoWidget";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 
@@ -61,6 +62,7 @@ export default function OrderContext() {
             </Box>
           )}
         </Box>
+        <InfoWidget label="Test-label" value="25" unit="ppm" />
       </PageContent>
     </div>
   );
