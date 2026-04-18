6dfb422fe3a50107790c0f3899810b5d7437664e
maeilert <maeilert@stud.ntnu.no>
2026-04-18 12:50:35 +0200

    feat: MachineCard UI component

--- web/src/entities/context/ui/index.ts
+++ web/src/entities/context/ui/index.ts
@@ -1,3 +1,4 @@
 export { RuleRow } from "./RuleRow";
 export { BucketBarChart } from "./BucketBarChart";
 export { BucketLineChart } from "./BucketLineChart";
+export { MachineCard } from "./MachineCard";
