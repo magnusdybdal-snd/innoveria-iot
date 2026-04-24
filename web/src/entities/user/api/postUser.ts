415eb3a30cd134d43e27a9017e1800c64ce450cf
Magnus Dybdal <magndy@stud.ntnu.no>
2026-04-24 12:08:02 +0200

    fix: change return type to promise void

--- web/src/entities/user/api/postUser.ts
+++ web/src/entities/user/api/postUser.ts
@@ -1,28 +1,17 @@
-import type {
-  CreateUserRequest,
-  UserApiResponse,
-} from "@entities/user/model/userSchema.ts";
+import type { CreateUserRequest } from "@entities/user/model/userSchema.ts";
 import { apiRequest, serviceClient } from "@shared/api";
 import { API_ROUTES } from "@shared/api/routes";
 
 /**
  * Creates a new user.
  * @param userData - The user data to create
- * @returns The created UserApiResponse
  */
-export const postUser = async (
-  userData: CreateUserRequest,
-): Promise<UserApiResponse> => {
-  return await apiRequest<UserApiResponse>(
-    serviceClient,
-    API_ROUTES.users,
-    "POST",
-    {
-      company_id: userData.companyId,
-      name: userData.name,
-      email: userData.email,
-      password: userData.password,
-      role: userData.role,
-    },
-  );
+export const postUser = async (userData: CreateUserRequest): Promise<void> => {
+  await apiRequest<void>(serviceClient, API_ROUTES.users, "POST", {
+    company_id: userData.companyId,
+    name: userData.name,
+    email: userData.email,
+    password: userData.password,
+    role: userData.role,
+  });
 };
