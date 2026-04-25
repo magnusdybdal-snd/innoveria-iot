2e83e617f57fc0f56d2946ddc816138135542973
vinjar <vinjar.dev@gmail.com>
2026-04-25 14:07:07 +0200

    feat: added erp agent token to company info

--- web/src/entities/company/ui/CompanyInfo.tsx
+++ web/src/entities/company/ui/CompanyInfo.tsx
@@ -1,53 +1,58 @@
-import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 
+import { ActionMenu } from "@shared/ui/actionMenu";
+
 type InfoProps = {
+  companyId: string;
   name: string;
   address: string;
   created_at: string;
   updated_at: string;
   addUser: () => void;
+  onGenerateERPToken: (companyId: string) => void;
 };
 
 /**
  * Displays a single company row's data: name, address, date created, and date updated.
  * Includes an ActionMenu for adding FACTORY_SUPERUSER to the company.
  * @param props - Component props
+ * @param props.companyId
  * @param props.name - Display name of the company
  * @param props.address - Main address of company office
  * @param props.created_at - Date the company was added
  * @param props.updated_at - Date of most recent change/addition/deletion related to company
  * @param props.addUser - Button to add a new company admin user
+ * @param props.onGenerateERPToken
  * @returns A set of grid-aligned cells with an action menu and rename dialog
  */
 export function CompanyInfo({
+  companyId,
   name,
   address,
   created_at,
   updated_at,
   addUser,
+  onGenerateERPToken,
 }: InfoProps) {
+  const menuItems = [
+    {
+      label: "Generate ERP token",
+      onClick: () => onGenerateERPToken(companyId),
+    },
+    {
+      label: "Add first company admin",
+      onClick: addUser,
+      disabled: true,
+    },
+  ];
+
   return (
     <>
       <Typography>{name}</Typography>
       <Typography>{address}</Typography>
       <Typography>{created_at}</Typography>
       <Typography>{updated_at}</Typography>
-      <Button
-        variant="outlined"
-        sx={{
-          backgroundColor: "primary.main",
-          color: "primary.dark",
-          "&:hover": { backgroundColor: "primary.main" },
-          borderRadius: 2,
-          textTransform: "none",
-          fontSize: 15,
-        }}
-        onClick={addUser}
-        // TODO: say first only when no company admin has been added.
-      >
-        Add first company admin
-      </Button>
+      <ActionMenu items={menuItems} />
     </>
   );
 }
