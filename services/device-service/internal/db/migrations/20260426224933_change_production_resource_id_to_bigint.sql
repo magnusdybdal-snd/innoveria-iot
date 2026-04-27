d68a51583b1016b715cf5b6c7f6126cf0203b59e
maeilert <maeilert@stud.ntnu.no>
2026-04-27 11:06:49 +0200

    feat: add goose up/down

--- services/device-service/internal/db/migrations/20260426224933_change_production_resource_id_to_bigint.sql
+++ services/device-service/internal/db/migrations/20260426224933_change_production_resource_id_to_bigint.sql
@@ -1,4 +1,9 @@
+-- +goose Up
 -- Change production_resource_id from uuid to bigint to match the ERP domain model
 -- where ProductionResource.ID is an int64 assigned by Monitor ERP.
 ALTER TABLE device.sensor
     ALTER COLUMN production_resource_id TYPE BIGINT USING NULL;
+
+-- +goose Down
+ALTER TABLE device.sensor
+    ALTER COLUMN production_resource_id TYPE UUID USING NULL;
