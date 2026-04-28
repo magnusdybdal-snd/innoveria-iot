99b89a262de1c9eff8260ad341b187fd9d8be8cb
Magnus Dybdal <magndy@stud.ntnu.no>
2026-04-28 14:27:45 +0200

    feat: wrap handlers in admin middleware

--- services/collection-service/internal/server/router.go
+++ services/collection-service/internal/server/router.go
@@ -4,6 +4,7 @@ package server
 import (
 	"innoveria-iot/collection-service/internal/domain"
 	"innoveria-iot/collection-service/internal/handlers"
+	"innoveria-iot/pkg/middleware"
 	"net/http"
 
 	_ "innoveria-iot/collection-service/docs"
@@ -23,7 +24,7 @@ func NewRouter(svc domain.MeasurementService, tenantMappingSvc domain.TenantMapp
 	mux.HandleFunc("GET "+MEASUREMENTS_BY_TIME, handlers.HandleMeasurementsByTimeRange(svc))
 
 	// Payload tags
-	mux.HandleFunc("GET "+PAYLOAD_TAGS, handlers.HandlePayloadTags(svc))
+	mux.HandleFunc("GET "+PAYLOAD_TAGS, middleware.AdminGuard(handlers.HandlePayloadTags(svc)))
 
 	// Company config routes:
 	mux.HandleFunc("POST "+COMPANY_CONFIG_ROUTE, handlers.PostTenantMapping(tenantMappingSvc))
