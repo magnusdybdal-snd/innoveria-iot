0a3ceda35ba6d9e5d6682bb0a3605dbddd0649f9
Magnus Dybdal <magndy@stud.ntnu.no>
2026-05-01 10:43:17 +0200

    feeat: add exports

--- web/src/entities/gateway/api/index.ts
+++ web/src/entities/gateway/api/index.ts
@@ -1,3 +1,4 @@
 export { getGateways } from "./getGateways.ts";
 export { postGateway } from "./postGateway.ts";
 export { deleteGateway } from "./deleteGateway.ts";
+export { patchGateway } from "./patchGateway.ts";
